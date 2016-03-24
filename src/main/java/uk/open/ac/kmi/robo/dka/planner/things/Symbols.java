1263feac4afd7c2f2a1eb3566d3b73fa4f98be76
enridaga <enricodaga@gmail.com>
2016-03-24 17:14:03 +0000

    Small changes

--- src/main/java/uk/open/ac/kmi/robo/dka/planner/things/Symbols.java
+++ src/main/java/uk/open/ac/kmi/robo/dka/planner/things/Symbols.java
@@ -15,15 +15,18 @@ public final class Symbols {
 	public static final Property At = new At();
 	public static final Validity Forever = new Forever();
 	public static final QuadResource Location = aQuadResource("Location");
-	public static final QuadProperty hasLocation = new QuadPropertyImpl("hasLocation");
-	public static final QuadProperty hasHumidity = new QuadPropertyImpl("hasHumidity");
-	public static final QuadProperty hasPeopleCount = new QuadPropertyImpl("hasPeopleCount");
-	public static final QuadProperty hasWiFiSignal = new QuadPropertyImpl("hasWiFiSignal");
-	public static final QuadProperty type = new QuadPropertyImpl("type");
-	public static final QuadProperty hasTemperature = new QuadPropertyImpl("hasTemperature");
+	public static final QuadProperty hasLocation = aQuadProperty("hasLocation");
+	public static final QuadProperty hasHumidity =  aQuadProperty("hasHumidity");
+	public static final QuadProperty hasPeopleCount =  aQuadProperty("hasPeopleCount");
+	public static final QuadProperty hasWiFiSignal =  aQuadProperty("hasWiFiSignal");
+	public static final QuadProperty type =  aQuadProperty("type");
+	public static final QuadProperty hasTemperature = aQuadProperty("hasTemperature");
 	public static final Wildcard _ = new Wildcard();
 
 	public static final QuadResource aQuadResource(String id) {
 		return new QuadResourceImpl(id);
+	}
+	public static final QuadProperty aQuadProperty(String id) {
+		return new QuadPropertyImpl(id);
 	}
 }
