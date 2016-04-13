40c853dcacd7e70b29bca3852a89b1e7bbb166ed
enridaga <enricodaga@gmail.com>
2016-04-13 15:57:41 +0100

    Adjusted scripts a files

--- server/dka.sh
+++ server/dka.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+flags=""
 
-# $1 is expected to be one of: query, update
-curl -G http://localhost:8080/$1 --data-urlencode $1="$2"
+
+if [ $1 = "problem" ]; then
+   curl $flags -G http://localhost:8080/planner/problem --data-urlencode query="$2"
+elif [ $1 = "load" ]; then
+   curl $flags -X PUT -T $2 http://localhost:8080/data -H "Content-type: application/n-quads; charset=utf-8"
+else
+   # $1 is expected to be one of: query, update
+   curl $flags -G http://localhost:8080/$1 --data-urlencode $1="$2"
+fi
+
